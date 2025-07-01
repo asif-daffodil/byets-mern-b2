56ea28b15c7b2e3127bc7fe835e7c92f01bbb053
asif-daffodil <abir@dipti.com.bd>
2025-07-01 15:12:18 +0600

    Add package.json for Parcel setup with Bootstrap and FontAwesome

---
+++ day-25 jul-1-2025/parcel/js/script.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "../css/style.css";
+console.log("Hello, Parcel!");
